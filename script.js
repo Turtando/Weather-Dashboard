d267173e0f9fca71f9b4d5ba41c481be8d04901c
Chris Turton <christopheraturton@gmail.com>
2020-06-17 00:24:32 -0400

    update to script/html

--- script.js
+++ script.js
@@ -1,37 +1,52 @@
-//Variable to track the cities searched?
-// var searchedCities = 0;
+// *Pseudocode*
+// Retrieve user input in the search and convert that to a variable
+// Use the Variable to run an AJAX call (GET) to Open Weather.
+// Transfer the Open Weather object into the respected fields in our html
+// Generate the HTML content dynamically 
 alert("This script is connected!")
 
 // FUNCTION
 // ================================
-$("#search-term").on("click", function(event) {
+$("#search-city").on("click", function (event) {
     event.preventDefault();
-    alert("test")
+
     // Search term Variable
-    var cityName = "Philadelphia";
-    // $("#search-term").val();
+    var cityName = $("#city-name").val();
+
+
     // Url & Key Variables
     var apiKey = "79b626f9fb03c7bd7e2f18487784205a";
-    var queryUrl = "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + cityName + "&cnt={cnt}&appid=" + apiKey;
-    
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
+    // var queryURL2 = "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + cityName + "&cnt={cnt}&appid=" + apiKey;
+    // console.log(queryURL);
+    var weatherResults = parseInt($("#weather-section").val());
     // Our call to our API backend (Get)
     $.ajax({
-        url: queryUrl,
+        url: queryURL,
         method: "GET"
-    }).then(function(weatherdata){
-        var results = weatherdata.forecast;
+    }).then(function (response) {
+        var results = response;
         console.log(results)
+        console.log(results.city.name)
+        console.log(results.list[0].main.temp)
+        console.log(results.list[0].wind.speed)
+
+        for (var i = 0; i < weatherResults; i++) { 
+            var name = $("<h3>").text(results.city.name);
+            var temperature = $("<p>").text(results.list[0].main.temp);
+            var windSpeed = $("<p>").text(results.list[0].wind.speed);
+           
+            console.log(name)
+            console.log(temperature);
+            console.log(windSpeed);
+            
+            $("weather-section").append(temperature, windSpeed, name)
+        }   
+            
     })
 })
-
-
 
 //  MAIN PROCESSES
 // ================================
 
 
-
-// Retrieve user input in the search and convert that to a variable
-// Use the Variable to run an AJAX call (GET) to Open Weather.
-// Transfer the Open Weather object into the respected fields in our html
-// Generate the HTML content dynamically 
