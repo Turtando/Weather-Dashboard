d9fe91661a650514fd66d047a648b414a6851334
Chris Turton <christopheraturton@gmail.com>
2020-06-17 21:37:51 -0400

    git commit -m "update to all files and readme" :wq!

--- Assets/script.js
+++ Assets/script.js
@@ -1,39 +1,39 @@
-// *Pseudocode*
-
-
-
-
-alert("This script is connected!")
-
 // GLOBAL FUNCTION
 // ================================
-
+// THIS IS THE FUNCTION WE WILL USE TO ALLOW OUR SEARCHED CITY BUTTONS TO CALL TO OUR API 
 let arrCities = [];
+
 function clickedButton(cityId) {
     var cityName = cityId
-
+    // var lat = results.city.coord.lat;
 
     // Url & Key Variables
     var apiKey = "79b626f9fb03c7bd7e2f18487784205a";
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial" + "&appid=" + apiKey;
     // var queryURL2 = "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + lat + "&lon=" + lon;
-    
+
     // run an AJAX call (GET) to Open Weather API backend.
-    $.ajax({
+    var ajax1 = $.ajax({
         url: queryURL,
         method: "GET"
-        //Our Then Function 
-    }).then(function (response) {
+
+    })
+    // var ajax2 = $.ajax({ 
+    //    url: queryURL2,
+    //    method: "GET"
+
+    //   });
+    $.when(ajax1, ).done(function (response) {
 
         var results = response;
-        
+
         $("#weather-section").empty();
         $("#day2").empty();
         $("#day3").empty();
         $("#day4").empty();
         $("#day5").empty();
         $("#day6").empty();
-        
+
         // Insert method/function here
         // Generate the HTML content dynamically 
 
@@ -44,10 +44,9 @@ function clickedButton(cityId) {
         var temperature = $("<p>").html(results.list[0].main.temp);
         var humidity = $("<p>").html(results.list[0].main.humidity);
         var windSpeed = $("<p>").html(results.list[0].wind.speed);
-       
-        
+
         // Day 2
-        var date2 = $("<p>").html(results.list[6].dt_txt); 
+        var date2 = $("<p>").html(results.list[6].dt_txt);
         var image2 = $("<img>").attr("src", "https://openweathermap.org/img/w/" + results.list[6].weather[0].icon + ".png")
         var temperature2 = $("<p>").html(results.list[6].main.temp);
         var humidity2 = $("<p>").html(results.list[6].main.humidity);
@@ -76,19 +75,15 @@ function clickedButton(cityId) {
         var temperature6 = $("<p>").html(results.list[38].main.temp);
         var humidity6 = $("<p>").html(results.list[38].main.humidity);
 
-
-
         // Transfer the Open Weather object into the respected fields in our html
-        $("#weather-section").append(name, date, image, temperature, humidity, windSpeed)
-        $("#day2").append(date2, image2, temperature2, humidity2)
-        $("#day3").append(date3, image3, temperature3, humidity3)
-        $("#day4").append(date4, image4, temperature4, humidity4)
-        $("#day5").append(date5, image5, temperature5, humidity5)
-        $("#day6").append(date6, image6, temperature6, humidity6)
-       
+        $("#weather-section").append(name, image, date, "Temp (in &#8457;):", temperature, "Humidity (%):", humidity, "Windspeed (in Mph):", windSpeed)
+        $("#day2").append(date2, image2, "Temp (in &#8457;):", temperature2, "Humidity (in &#37;):", humidity2)
+        $("#day3").append(date3, image3, "Temp (in &#8457;):", temperature3, "Humidity (in &#37;):", humidity3)
+        $("#day4").append(date4, image4, "Temp (in &#8457;):", temperature4, "Humidity (in &#37;):", humidity4)
+        $("#day5").append(date5, image5, "Temp (in &#8457;):", temperature5, "Humidity (in &#37;):", humidity5)
+        $("#day6").append(date6, image6, "Temp (in &#8457;):", temperature6, "Humidity (in &#37;):", humidity6)
     })
 };
-
 
 $("#search-city").on("click", function (event) {
     event.preventDefault();
@@ -102,36 +97,37 @@ $("#search-city").on("click", function (event) {
     var apiKey = "79b626f9fb03c7bd7e2f18487784205a";
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial" + "&appid=" + apiKey;
     // var queryURL2 = "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + lat + "&lon=" + lon;
-    
+
+    // console.log(queryURL2)
     // run an AJAX call (GET) to Open Weather API backend.
-    $.ajax({
+
+    var ajax1 = $.ajax({
         url: queryURL,
         method: "GET"
-        //Our Then Function 
-    }).then(function (response) {
 
+    })
+    // var ajax2 = $.ajax({ 
+    //    url: queryURL2,
+    //    method: "GET"
+
+    //   });
+    $.when(ajax1,).done(function (response) {
+        // this for loop will determine whether or not to push searched cities into our array
         console.log(arrCities)
         var repeat = false;
-
         for (var i = 0; i < arrCities.length; i++) {
             if (arrCities[i] === cityName) {
                 repeat = true;
             }
         }
-       
         if (repeat === false) {
             arrCities.push(cityName)
-            $("#searched-city").append("<button id=' "+ cityName + "' onClick='clickedButton(this.id)'" + "class='cityButton'>" + cityName + "</button>");
-
-            
+            $("#searched-city").append("<button id=' " + cityName + "' onClick='clickedButton(this.id)'" + "class='cityButton'>" + cityName + "</button>");
         }
         console.log(arrCities)
         var results = response;
-        // console.log(results)
-        // console.log(results.city.name)
-        // console.log(results.list[0].main.temp)
-        // console.log(results.list[0].main.humidity)
-        // console.log(results.list[0].wind.speed)
+        console.log(results)
+
         // We use this to empty our Weather Section so that we can refresh the section before new content is appended
         $("#weather-section").empty();
         $("#day2").empty();
@@ -139,7 +135,7 @@ $("#search-city").on("click", function (event) {
         $("#day4").empty();
         $("#day5").empty();
         $("#day6").empty();
-        
+
         // Insert method/function here
         // Generate the HTML content dynamically 
 
@@ -150,61 +146,48 @@ $("#search-city").on("click", function (event) {
         var temperature = $("<p>").html(results.list[0].main.temp);
         var humidity = $("<p>").html(results.list[0].main.humidity);
         var windSpeed = $("<p>").html(results.list[0].wind.speed);
-       
-        
         // Day 2
-        var date2 = $("<p>").html(results.list[6].dt_txt); 
+        var date2 = $("<p>").html(results.list[6].dt_txt);
         var image2 = $("<img>").attr("src", "https://openweathermap.org/img/w/" + results.list[6].weather[0].icon + ".png")
         var temperature2 = $("<p>").html(results.list[6].main.temp);
         var humidity2 = $("<p>").html(results.list[6].main.humidity);
-
         // Day 3
         var date3 = $("<p>").html(results.list[14].dt_txt);
         var image3 = $("<img>").attr("src", "https://openweathermap.org/img/w/" + results.list[14].weather[0].icon + ".png")
         var temperature3 = $("<p>").html(results.list[14].main.temp);
         var humidity3 = $("<p>").html(results.list[14].main.humidity);
-
         // Day 4
         var date4 = $("<p>").html(results.list[22].dt_txt);
         var image4 = $("<img>").attr("src", "https://openweathermap.org/img/w/" + results.list[22].weather[0].icon + ".png")
         var temperature4 = $("<p>").html(results.list[22].main.temp);
         var humidity4 = $("<p>").html(results.list[22].main.humidity);
-
         // Day 5
         var date5 = $("<p>").html(results.list[30].dt_txt);
         var image5 = $("<img>").attr("src", "https://openweathermap.org/img/w/" + results.list[30].weather[0].icon + ".png")
         var temperature5 = $("<p>").html(results.list[30].main.temp);
         var humidity5 = $("<p>").html(results.list[30].main.humidity);
-
         // Day 6
         var date6 = $("<p>").html(results.list[38].dt_txt);
         var image6 = $("<img>").attr("src", "https://openweathermap.org/img/w/" + results.list[38].weather[0].icon + ".png")
         var temperature6 = $("<p>").html(results.list[38].main.temp);
         var humidity6 = $("<p>").html(results.list[38].main.humidity);
 
-
-
         // Transfer the Open Weather object into the respected fields in our html
-        $("#weather-section").append(name, date, image, temperature, humidity, windSpeed)
-        $("#day2").append(date2, image2, temperature2, humidity2)
-        $("#day3").append(date3, image3, temperature3, humidity3)
-        $("#day4").append(date4, image4, temperature4, humidity4)
-        $("#day5").append(date5, image5, temperature5, humidity5)
-        $("#day6").append(date6, image6, temperature6, humidity6)
-       
+        $("#weather-section").append(name, image, date, "Temp (in &#8457;):", temperature, "Humidity (%):", humidity, "Windspeed (in Mph):", windSpeed)
+        $("#day2").append(date2, image2, "Temp (in &#8457;):", temperature2, "Humidity (in &#37;):", humidity2)
+        $("#day3").append(date3, image3, "Temp (in &#8457;):", temperature3, "Humidity (in &#37;):", humidity3)
+        $("#day4").append(date4, image4, "Temp (in &#8457;):", temperature4, "Humidity (in &#37;):", humidity4)
+        $("#day5").append(date5, image5, "Temp (in &#8457;):", temperature5, "Humidity (in &#37;):", humidity5)
+        $("#day6").append(date6, image6, "Temp (in &#8457;):", temperature6, "Humidity (in &#37;):", humidity6)
     })
-
 })
+//Saving Function
+var arrSearchedCities = JSON.parse(localStorage.getItem("text")) 
+$("#search-city").on("click", function () {
+    var Cities = $("#city-name").val()
 
-
-//  Activate Searched Cities
-// ================================
-// $(document).on("click", '#searched-city .cityButton', function() {
-//     // event.preventDefault();
-    
-//     var fired_button = $(this).val();
-//     alert(fired_button);
-
-
-// });
-
+    arrSearchedCities.push({
+        cities: Cities,
+    })
+    localStorage.setItem("text", JSON.stringify(arrSearchedCities));
+})
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Weather-Dashboard
+
+For this particular repository, I have developed a live and functioning weather dashboard. In this particular project, my objectives and goals were to be able to create a viable weather gathering function that was to retrieve data from another API (application program interface). In this instance the data we are retrieving in this application is coming directly from the OpenWeatherMap API. 
+
+In this app specifically, the user will be able to generate a 5-day weather outlook for any city that they choose. When they enter the city into the search bar, they are presented with conditions that give the City Name and date of the time they are searching along with an icon that represents the current and future weather conditions, temperature, humidity percentage and wind speed. 
+
+Even after a new city has been searched by the user, the searched cities will remain on a list under the search section- if the user clicks on any of the cities, her or she will be able to see that city's forecast. 
+
+Thank you,
+Chris Turton
+
+Gif of Demo below:
+
+[](./assets/weatherdashboarddemo.gif)
+https://turtando.github.io/Weather-Dashboard / https://github.com/Turtando?tab=repositories
